6f1f052b9ba45c04478d9da023096641671ce02d
Edward Kearn <ejkearn@gmail.com>
2018-05-01 15:24:37 -0600

    added new functionality

--- itunes-controller.js
+++ itunes-controller.js
@@ -1,19 +1,61 @@
-function ItunesController(){
+function ItunesController() {
   var itunesService = new ItunesService()
   //Do Not Modify the getMusic function
-  this.getMusic = function getMusic(e){
+  this.getMusic = function getMusic(e) {
     e.preventDefault();
     var artist = e.target.artist.value;
     itunesService.getMusicByArtist(artist).then(draw); //after get music by artist returns what are you doing with the objects?
   }
-
+  var playing = false;
   //Start coding here
-  function draw(results){
+  function draw(results) {
+    var template = '';
+    for (let i = 0; i < results.length; i++) {
+      var card = results[i];
+      var custImg = card.albumArt.replace('100x100', '250x250')
+      template += `<div class="card audioCard text-center" >
+      <img class="card-img-top songImg" src="${custImg}" alt="Card image cap" onclick="app.controllers.itunesCtrl.playPause('${card.preview}')">
+      <div class="card-body">
+        <h5 class="card-title">Song Title: ${card.title}</h5>
+        <p class="card-text">Artist: ${card.artist}</p>
+        <p class="card-text">Collection: ${card.collection}</p>
+        <p class="card-text">Price: ${card.price}</p>
+
+      </div>
+    </div>`
+    }
+    var songElem = document.getElementById('songs');
+    songElem.innerHTML = template;
     console.log(results)
   }
 
+  var song = new Audio();
+
+  this.playPause = function playPause(src) {
+
+    if (playing) {
+      pauseSong(src)
+      if (src !== song.src) {
+        playSong(src)
+      }
+
+    } else {
+      playSong(src)
+    }
 
 
 
-  
+  }
+  function playSong(src) {
+
+    song.src = src,
+      song.play();
+    playing = true
+  }
+  function pauseSong(src) {
+
+    song.pause();
+    playing = false
+  }
+
 }
--- itunes-service.js
+++ itunes-service.js
@@ -3,7 +3,7 @@ function ItunesService(){
     this.getMusicByArtist = function(artist) {
 
       //allows requests to localhost: 8080 otherwise blocked by itunes
-      var url = 'https://itunes.apple.com/search?term=' + artist;
+      var url = 'https://itunes.apple.com/search?term=' + artist+'&media=music';
       
       //changes the button to loading while songs load
       $('#get-music-button').text('LOADING....');
@@ -13,7 +13,7 @@ function ItunesService(){
         var songList = response.results.map(function (song) {
                   return {
                       title: song.trackName,
-                      albumArt: song.artworkUrl60,
+                      albumArt: song.artworkUrl100,
                       artist: song.artistName,
                       collection: song.collectionName,
                       price: song.collectionPrice,
